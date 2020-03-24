ac0a7106c6e798c70a122237f89d3ff9ceda44e7
abhishek <abhishek.satapathy01@gmail.com>
2020-03-24 21:56:41 +0530

    Tried out gym and created q-table

---
+++ q-Learning.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+env = gym.make("MountainCar-v0")
+env.reset()
+
+print(env.observation_space.high)
+print(env.observation_space.low)
+print(env.action_space.n)
+
+# done = False
+
+# while not done:
+#     action = 2
+#     new_state, reward, done , _ = env.step(action)
+#     env.render()
+#     print(done)
+# env.close()
+
+
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+
+print(discrete_os_win_size)
+
+q_table = np.random.uniform(low = -2, high = 0, size = (DISCRETE_OS_SIZE + [env.action_space.n]))
+print(q_table.shape)
+print(q_table)
